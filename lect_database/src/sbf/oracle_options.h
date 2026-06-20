7c8c641f2d8f7b076e4a3205a3f32129432ac104
TIAN <332977694@qq.com>
2026-06-20 11:00:58 +0800

    Centralize oracle debug env flags

--- lect_database/src/sbf/oracle_options.h
+++ lect_database/src/sbf/oracle_options.h
@@ -4,18 +4,35 @@
 
 namespace rbf {
 
+enum class OracleDebugEnvMode {
+    Present,
+    EqualsOne,
+};
+
+inline bool oracle_debug_env_enabled(const char* name, OracleDebugEnvMode mode) {
+    const char* value = std::getenv(name);
+    if (value == nullptr) {
+        return false;
+    }
+    switch (mode) {
+        case OracleDebugEnvMode::Present:
+            return true;
+        case OracleDebugEnvMode::EqualsOne:
+            return value[0] == '1';
+    }
+    return false;
+}
+
 inline bool oracle_best_tighten_debug_enabled() {
-    return std::getenv("SBF_BT_DEBUG") != nullptr;
+    return oracle_debug_env_enabled("SBF_BT_DEBUG", OracleDebugEnvMode::Present);
 }
 
 inline bool oracle_canonical_debug_enabled() {
-    const char* value = std::getenv("RBF_CANONICAL_DEBUG");
-    return value != nullptr && value[0] == '1';
+    return oracle_debug_env_enabled("RBF_CANONICAL_DEBUG", OracleDebugEnvMode::EqualsOne);
 }
 
 inline bool oracle_envelope_debug_enabled() {
-    const char* value = std::getenv("RBF_ENV_DEBUG");
-    return value != nullptr && value[0] == '1';
+    return oracle_debug_env_enabled("RBF_ENV_DEBUG", OracleDebugEnvMode::EqualsOne);
 }
 
 }  // namespace rbf
